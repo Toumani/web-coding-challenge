63056f69153ef8a732d707d383a04b5c6e2eed0a
Toumani <toumani49@gmail.com>
2018-11-27 20:24:01 +0100

    Added validation to login page

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -9,6 +9,8 @@ import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 import { FormControl } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import { HelpBlock } from 'react-bootstrap';
+import { FormGroup } from 'react-bootstrap';
 
 // CSS import
 import './Form.css';
@@ -18,8 +20,44 @@ class LoginForm extends Component {
 		super(props);
 		
 		this.state = {
-			view: 'login'
+			emailError: '',
+			emailValidation: null,
+			displayEmailError: false
 		};
+	}
+
+	/**
+	 * Checks the email input against regex and @returns true if correct
+	 */
+	checkEmail(email) {
+		var emailRegex = /^[\w\.-_]{2,}@\w{2,}\.\w{2,}$/;
+		return (email.match(emailRegex));
+	}
+
+	/**
+	 * Sets email input form group validation state
+	 * Trigerred when email input value is changed after first validation
+	 * so user doesn't have error message before finishing typing
+	 */
+	setEmailError = (e) => {
+		if (this.state.displayEmailError) {
+			var errorMessage = this.checkEmail(e.target.value) ? '' : 'Please enter a valid email address';
+			this.setState({ emailError: errorMessage});
+		}
+	}
+
+	/**
+	 * Set email input form group validaiton state
+	 * Trigerred when email input value is changed
+	 */
+	setEmailState = (e) => {
+		this.setEmailError(e);
+		if (this.checkEmail(e.target.value)) {
+			this.setState({ emailValidation: 'success', displayEmailError: true })
+		}
+		else {
+			this.setState({ emailValidation: 'error' })
+		}
 	}
 
 	render() {
@@ -32,10 +70,14 @@ class LoginForm extends Component {
 					<Grid className="form-content">
 						<Row>
 							<Col md={12}>
-								<FormControl
-									type="email"
-									placeholder="Email"
-								/>
+								<FormGroup validationState={ this.state.emailValidation }>
+									<FormControl
+										type="email"
+										placeholder="Email"
+										onChange={ this.setEmailState }
+									/>
+									<HelpBlock>{ this.state.emailError }</HelpBlock>
+								</FormGroup>
 							</Col>
 						</Row>
 						<Row>
