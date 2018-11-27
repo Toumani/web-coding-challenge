9d3b5ebc6a23bc974cc02340ea749973c4a02f34
Toumani <toumani49@gmail.com>
2018-11-27 14:52:33 +0100

    Project initialized

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// React-Bootstrap components
+import { Button } from 'react-bootstrap';
+
+class Header extends Component {
+	render() {
+		return (
+			<header className="App-header">
+				Shopzz
+			</header>
+		);
+	}
+}
+
+export default Header;
