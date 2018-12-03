66451e020848666281ce7ec1ca0f941b3283295f
toumani <toumani49@gmail.com>
2018-12-03 14:36:55 +0100

    Added nearby and favorite shop request feature

--- src/AppContent.js
+++ src/AppContent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Grid, Row, Col } from 'react-bootstrap';
+import { Button, Grid, Row, Col, PageHeader } from 'react-bootstrap';
 
 import Shop from './Shop';
 import Links from './Links';
@@ -12,7 +12,8 @@ class AppContent extends Component {
 		super(props);
 
 		this.state = {
-			shops: null
+			shops: null,
+			view: 'nearby'
 		}
 	}
 
@@ -28,7 +29,6 @@ class AppContent extends Component {
 	 * A shop is defined by an array of objects containing the name, the distance and the image of the shop
 	 */
 	getNearbyShops() {
-		// var data = {};
 		var data = "{\
 			\"hashcode\": \"b8adf586687809a7d4d6eb61f62549209e218c75\",\
 			\"location\": {\
@@ -46,66 +46,29 @@ class AppContent extends Component {
 			},
 			dataType: 'JSON',
 			encode: true,
-			success: function(response, status, xhr) {
+			success: (response, status, xhr) => {
 				console.log("Fine");
-				this.setState({ shops: response });
+				console.log(response);
+				this.setState({ shops: response, view: 'nearby' });
 			},
 			error: function(xhr, status, error) {
-			  console.log("Something went wrong!");
+				console.log("Something went wrong!");
 			}
-		  });
+		});
+	}
 
-		var settings1 = {
-			"async": true,
-			"crossDomain": true,
-			"url": "http://localhost:9090/shops",
-			"method": "POST",
-			"headers": {
-			  "Content-Type": "application/json",
-			  "cache-control": "no-cache",
-			},
-			"data": {
-				hashcode: "b8adf586687809a7d4d6eb61f62549209e218c75",
-				location: {
-					latitude: 32.288742,
-					longitude: -9.236141
-				}
-			},
-			"success": function(response) {
-				console.log('Done');
-				console.log(response);
-			},
-			"error": function (xhr, status, error) {
-				console.log('No way! Why isn\'t that sh*t working !!!');
-			}
-		  }
+	getNearbyShopsFromEvent = (e) => {
+		this.getNearbyShops();
+	}
+
+	getFavoriteShopsFromEvent = (e) => {
+		this.getFavoriteShops();
 	}
 
 	/**
 	 * This is a debugging function. Acutally, server handles refleshed list
 	 */
 	getRefreshedNearbyShops = (e) => {
-		// Mocking
-		this.setState({ shops:
-			[
-				{
-					name: "Strings",
-					distance: 7,
-					image: "./Strings.jpg"
-				},
-				{
-					name: "Wind",
-					distance: 17,
-					image: "./Strings.jpg"
-				}
-			]
-		});
-	}
-
-	/**
-	 * Request favorite shops from server
-	 */
-	getRefreshedFavoriteShops = (e) => {
 		// Mocking
 		this.setState({ shops:
 			[
@@ -118,25 +81,62 @@ class AppContent extends Component {
 		});
 	}
 
+	/**
+	 * Request favorite shops from server
+	 */
+	getFavoriteShops = (e) => {
+		var data = "{\
+			\"hashcode\": \"b8adf586687809a7d4d6eb61f62549209e218c75\",\
+			\"location\": {\
+				\"latitude\": 32.288742,\
+				\"longitude\": -9.236141\
+			}\
+		}";
+		$.ajax({
+			type: 'POST',
+			url: "http://localhost:9090/favorite",
+			processData: false,
+			data: data,
+			headers: {
+				"Content-Type": "application/json"
+			},
+			dataType: 'JSON',
+			encode: true,
+			success: (response, status, xhr) => {
+				console.log("Fine");
+				console.log(response);
+				this.setState({ shops: response, view: 'favorite' });
+			},
+			error: function(xhr, status, error) {
+				console.log("Something went wrong!");
+			}
+		});
+	}
+
 	render() {
 		var shops = null;
 		if (this.state.shops !== null) {
 			shops = this.state.shops.map((shop) =>
 				<Col md={ 4 } sm={ 6 } xs={ 12 }>
 					<Shop
-						name={ shop.name}
-						distance={ shop.distance }
+						name={ shop.name }
+						distance={ Math.round(shop.distanceToUser*10)/10 }
 						image={ shop.image }
 
-						liked={ this.getRefreshedNearbyShops }
-						disliked={ this.getRefreshedNearbyShops }
+						liked={ this.getNearbyShopsFromEvent }
+						disliked={ this.getNearbyShopsFromEvent }
+
+						view={ this.state.view }
 					/>
 				</Col>
 			);
 		}
 		return (
 			<div>
-				<Links nearby={ this.getRefreshedNearbyShops } favorite={ this.getRefreshedFavoriteShops }/>
+				<PageHeader>
+					{ this.state.view === 'nearby' ? 'Nearest shops. Favorite and disliked aren\'t shown' : 'Favorite shops' }
+				</PageHeader>
+				<Links nearby={ this.getNearbyShopsFromEvent } favorite={ this.getFavoriteShopsFromEvent }/>
 				<Grid>
 					<Row>
 						{ shops }
--- src/Shop.js
+++ src/Shop.js
@@ -4,11 +4,17 @@ import { Thumbnail, Button, Glyphicon } from 'react-bootstrap';
 import './Shop.css';
 
 class Shop extends React.Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			view: this.props.view
+		}
+	}
 	render() {
-		return (
-			<Thumbnail src={ this.props.image } alt={ this.props.name }>
-				<h3>{ this.props.name }</h3>
-				<p>{ this.props.distance } km</p>
+		var buttonContainer;
+		if (this.props.view === 'nearby')
+			buttonContainer = (
 				<p className="button-container">
 					<Button bsSize="large" onClick={ this.props.liked }>
 						<Glyphicon glyph="heart" /> Like
@@ -17,6 +23,21 @@ class Shop extends React.Component {
 						<Glyphicon glyph="thumbs-down" /> Dislike
 					</Button>
 				</p>
+			);
+		else {
+			buttonContainer = (
+				<p className="button-container">
+					<Button bsSize="large" onClick={ this.props.liked }>
+						<Glyphicon glyph="minus" /> Remove
+					</Button>
+				</p>
+			)
+		}
+		return (
+			<Thumbnail src={ this.props.image } alt={ this.props.name }>
+				<h3>{ this.props.name }</h3>
+				<p>{ this.props.distance } km</p>
+				{ buttonContainer }
 			</Thumbnail>
 		);
 	}
